81f225b0808ee77a0e89f0b6ff87e8f76f415ebf
Luke Underwood <veryveryeasily@gmail.com>
2021-09-01 00:30:45 -0700

    think its working

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import config
 from flask import Flask
-from .development import Bot as TestBot
-from .production import Bot as MainBot
+from development import Bot as TestBot
+from production import Bot as MainBot
 import os
 import logging
 
@@ -9,19 +9,24 @@ logger = logging.getLogger("gunicorn.error")
 app = Flask(__name__)
 app.logger.handlers = logger.handlers
 logger.info("I'm working")
-test_bot_id = os.environ['DEV_GROUPME_BOT_ID']
-bot_id = os.environ['PROD_GROUPME_BOT_ID']
+test_bot_id = os.environ["DEV_GROUPME_BOT_ID"]
+bot_id = os.environ["PROD_GROUPME_BOT_ID"]
 
 test_bot = TestBot(test_bot_id)
 test_bot.run()
 main_bot = MainBot(bot_id)
 main_bot.run()
 
-@app.route('/test', methods=['POST'])
+
+@app.route("/test", methods=["POST"])
 def development():
     return test_bot.route()
 
-@app.route("/main", methods=['POST'])
+
+@app.route("/main", methods=["POST"])
 def production():
     return main_bot.route()
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
